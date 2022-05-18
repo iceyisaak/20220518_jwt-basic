c20d9112dc992d9796234e0d7c8d533f8be01a9c
iceyisaak <me@iceyisaak.com>
2022-05-18 23:43:52 +0200

    Added fatures

---
+++ controllers/main.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+const CustomAPIError = require('../errors/custom-error');
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    throw new CustomAPIError('Please provide the correct username and password.', 400);
+  }
+
+  // DEMO ID
+  const id = new Date().getDate();
+
+  // DEMO ONLY
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, { expiresIn: '30d' });
+  res.status(200).json({ msg: 'User Created', token });
+};
+
+const dashboard = async (req, res) => {
+
+  console.log(req.user);
+  const luckyNumber = Math.floor(Math.random() * 100);
+  res.status(200).json({ msg: `Hi, ${decoded.username}`, secret: `Here is your authorised data. Your lucky number is ${luckyNumber}` });
+
+
+
+};
+
+module.exports = {
+  login,
+  dashboard
+};
